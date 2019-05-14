6618e0eecfe9dd3155032b7342a9511471cd3022
Thanos Restas <thanosre@gmail.com>
2019-05-14 03:00:47 +0300

    "Shooting" by mouse picking, loading models via scene edited by the editor.

--- src/index.js
+++ src/index.js
@@ -17,95 +17,52 @@ class Engine{
         this.scene = new BABYLON.Scene(this.engine);
         
         // Camera setup
-        this.camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, -7.99, -20), this.scene);        
+        this.camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 2.01, -8), this.scene);        
         this.camera.attachControl(this.canvas, true);
         this.camera.speed = 0.2;       
         this.camera.ellipsoid = new BABYLON.Vector3(1, 1, 1); // Collision box for the camera
         this.camera.checkCollisions = true;
         this.camera.applyGravity = true; 
-        this.canJump = true;      
 
         // Enable collisions and gravity in scene
         this.scene.collisionsEnabled = true
-        this.scene.gravity = new BABYLON.Vector3(0, -0.2, 0)  
+        this.scene.gravity = new BABYLON.Vector3(0, -1, 0)  
     }
-
-    // Unused
-    checkJump(context){
-        // Check if player/camera is on ground/crate and set its ability to jump        
-        this.camera.onCollide = function (colMesh) {
-            if (colMesh.uniqueId === context.scene.getMeshByName("ground").uniqueId || colMesh.uniqueId === context.scene.getMeshByName("crate".uniqueId)) {          
-                context.camera.canJump = true;
-            }
-        }    
-    }    
 }
 
 function main(){
-    game = new Engine()
-    
+    game = new Engine();      
+}
+
+
+function assetLoader(){
     // Add lights to the scene
     var light0 = new BABYLON.DirectionalLight("Omni", new BABYLON.Vector3(-2, -5, 2), game.scene)
-    var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(2, -5, -2), game.scene)
-    
-    // Add ground with grid texture   
-    var ground = BABYLON.Mesh.CreatePlane("ground", 20.0, game.scene)
-    ground.material = new GridMaterial("groundMaterial", game.scene)    
-    ground.material.diffuseColor = new BABYLON.Color3(1, 1, 1)
-    ground.material.backFaceCulling = false
-    ground.position = new BABYLON.Vector3(5, -10, -15)
-    ground.rotation = new BABYLON.Vector3(Math.PI / 2, 0, 0)
+    var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(2, -5, -2), game.scene) 
 
-    // Finally, say which mesh will be collisionable   
-    ground.checkCollisions = true;
-
-    var assetsManager = new BABYLON.AssetsManager(game.scene);
-    
+    var assetsManager = new BABYLON.AssetsManager(game.scene);     
     // Called when a single task has been sucessfull
-    assetsManager.onTaskSuccessObservable.add(function(task) {
-        if(task.name == "task"){
-            task.loadedMeshes[0].parent = game.scene.getMeshByName("ground");
-            task.loadedMeshes[0].setPositionWithLocalVector(new BABYLON.Vector3(0, 8, 0)) // x == z , y == z, z == y from player's view
-            task.loadedMeshes[0].rotation = new BABYLON.Vector3(-Math.PI / 2, 0, 0) ;          
-    
-            //Simple invisble crate that acts as a hitbox to avoid expensive pixel-perfect collision
-            var box = BABYLON.MeshBuilder.CreateBox("myBox", {height: 1.5, width: 0.50, depth: 1}, game.scene);
-            box.isVisible = false;    
-            box.setPositionWithLocalVector(new BABYLON.Vector3(0, 1, 0))
-            box.showBoundingBox = true;
-            box.parent =  task.loadedMeshes[0];
-            box.checkCollisions = true;
-        }
-        else if( task.name == "task2"){
-            task.loadedMeshes[0].parent = game.scene.getMeshByName("ground");
-            task.loadedMeshes[0].setPositionWithLocalVector(new BABYLON.Vector3(-6, 8, -4)) // x == z , y == z, z == y from player's view
-            task.loadedMeshes[0].rotation = new BABYLON.Vector3(-Math.PI / 2, 0, 0) ; 
-            task.loadedMeshes[0].scaling = new  BABYLON.Vector3( 0.1,  0.1,  0.1) ;         
-        }
-        else if( task.name == "task3"){
-            task.loadedMeshes[0].parent = game.camera;
-            task.loadedMeshes[0].setPositionWithLocalVector(new BABYLON.Vector3(2, -1.5, 4)) // x == z , y == z, z == y from player's view
-            task.loadedMeshes[0].rotation = new BABYLON.Vector3( Math.PI, -0.2, 0) ; 
-            task.loadedMeshes[0].scaling = new  BABYLON.Vector3( 0.1,  0.1,  0.1) ;         
-        }
-        //console.log('task successful', task);
-    });
+    assetsManager.onTaskSuccessObservable.add(function(task) {        
+        //console.log("task successful", task);
 
+        // Setting ground material
+        var ground = game.scene.getMeshByName("ground");
+        ground.material = new GridMaterial("groundMaterial", game.scene)    
+        ground.material.diffuseColor = new BABYLON.Color3(1, 1, 1)
+        ground.material.backFaceCulling = false 
+    }); 
     // Called when all tasks in the assetsManger are done
     assetsManager.onTasksDoneObservable.add(function(tasks) {
-        var errors = tasks.filter(function(task) {return task.taskState === BABYLON.AssetTaskState.ERROR});
-        var successes = tasks.filter(function(task) {return task.taskState !== BABYLON.AssetTaskState.ERROR});
 
+        var errors = tasks.filter(function(task) {return task.taskState === BABYLON.AssetTaskState.ERROR});
+        var successes = tasks.filter(function(task) {return task.taskState !== BABYLON.AssetTaskState.ERROR}); 
         //console.log(tasks);
-    }); 
+    });
 
     // We add single tasks to the assetsManager
-    assetsManager.addMeshTask("task", "", "../assets/models/", "dummy.babylon");
-    assetsManager.addMeshTask("task2", "", "../assets/models/", "skull.babylon");
-    assetsManager.addMeshTask("task3", "", "../assets/models/", "deagle.obj");
-
+    assetsManager.addMeshTask("task", "", "../assets/models/", "test3.babylon"); 
     // Now let the assetsManger load/excecute every task
-    assetsManager.load();     
+    assetsManager.load();
 }
 
 
@@ -279,20 +236,25 @@ function cameraManager(){
 function render(){
     // Render every frame
     game.engine.runRenderLoop(() => {
-        game.scene.render()                  
+        game.scene.render()
+        //console.log(game.camera.position);                  
     })   
 }
 
-
-main();
 //When click event is raised
 window.addEventListener("click", function () {
     // We try to pick an object
     var pickResult = game.scene.pick(game.scene.pointerX, game.scene.pointerY);
-    //console.log(pickResult.pickedMesh.name);
-    //game.scene.getMeshByName.pickResult.pickedMesh.isVisible = true;
+    var model = pickResult.pickedMesh;
 
+    if(pickResult !== null && model !== null){
+        console.log(model.name);
+        game.scene.getMeshByName(model.name).dispose();
+    }
 }),
+
+main();
+assetLoader();
 pointerLock();
 cameraManager();
 render();
