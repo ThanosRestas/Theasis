759a52d183e69fdd5d5d2f3b74c86a73ddd0a73f
Thanos Restas <thanosre@gmail.com>
2019-10-03 18:32:55 +0300

    Added cartoon skybox

--- src/engine.js
+++ src/engine.js
@@ -105,7 +105,7 @@ export default class Engine{
             var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
             var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
             skyboxMaterial.backFaceCulling = false;
-            skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("../assets/textures/skybox/skyBox2w", scene);
+            skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("../assets/textures/skybox/cartoon", scene);
             skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
             skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
             skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
